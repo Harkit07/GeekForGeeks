061e49425d22c6ab5d0a278dded8c29e0c3781e1
HARKIT SINGH SRAN <harkitsinghsran9584@gmail.com>
2026-07-14 15:45:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Non Repeating Character/non-repeating-character.java
@@ -0,0 +1,18 @@
+class Solution {
+    public char nonRepeatingChar(String s) {
+        // code here
+        int freq[]= new int[26];
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            freq[ch-'a']++;
+        }
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(freq[ch-'a']==1){
+                return ch;
+            }
+        }
+        char notFound = '$';
+        return notFound;
+    }
+}
