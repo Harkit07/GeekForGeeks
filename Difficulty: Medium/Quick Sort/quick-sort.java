f381914d82d67bf5c7b7cff4e15141acce9e4e38
HARKIT SINGH SRAN <harkitsinghsran9584@gmail.com>
2026-08-07 14:44:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Quick Sort/quick-sort.java
@@ -0,0 +1,31 @@
+class Solution {
+    public void quickSort(int[] arr, int low, int high) {
+        // code here
+        if(low>=high){
+            return;
+        }
+        int pIdx=partition(arr, low, high);
+        quickSort(arr, low, pIdx-1);
+        quickSort(arr, pIdx+1, high);
+    }
+
+    private int partition(int[] arr, int low, int high) {
+        // code here
+        int pivot = arr[high];
+        int i=low-1;
+        
+        for(int j=low; j<high; j++){
+            if(arr[j]<=pivot){
+                i++;
+                int temp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+            }
+        }
+        i++;
+        int temp=pivot;
+        arr[high]=arr[i];
+        arr[i]=temp;
+        return i;
+    }
+}
